9fabd50fd844a9235d74fef57e761dfe5ea60c54
Keywhee <adorablefluffykitties@gmail.com>
2020-07-21 18:35:00 -0400

    i really hope this works :(

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let comments;
+const results = document.querySelector('#cmtslc');
 
 const fetchComments = async () => {
   comments = await fetch('/comments.json')
@@ -6,3 +7,39 @@ const fetchComments = async () => {
     .catch(err => err)
   return true;
 }
+
+function filterComments(page_id) {
+  let filtered = []
+  filtered = comments.filter(comment => {
+    return comment.id == page_id;
+  });
+  return filtered;
+}
+
+
+const showComments = async(comments) => {
+  const commentsMarkup = "${renderComments(comments)}"
+  results.innerHTML = commentsMarkup;
+}
+
+function renderComments(comments) {
+  return `
+    <ul>
+      ${comments.map(comment => `
+        <div class="a-comment">
+          <div class="a-comment-top">
+            <div class="a-comment-author">${comment.author_name}</div>
+            <div class="a-comment-date">${comment.comment_date}</div>
+          </div>
+          <div class="a-comment-bottom">
+            <div class="a-comment-comment">${comment.comment_content}</div>
+          </div>
+        </div>
+        `).join('')}
+    </ul>
+  `
+}
+
+(function() {
+  fetchComments();
+})
