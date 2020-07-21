c7132c40547591a15ce63b73236b85ace3c054cd
Keywhee <adorablefluffykitties@gmail.com>
2020-07-21 18:57:05 -0400

    i really hope this works

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 let comments;
-const results = document.querySelector('#cmtslc');
 
 const fetchComments = async () => {
   comments = await fetch('/comments.json')
@@ -7,6 +6,56 @@ const fetchComments = async () => {
     .catch(err => err)
   return true;
 }
+
+function addPost(author, date, content) {
+  return `
+    <div class="a-comment-top">
+      <div class="a-comment-author">${author}</div>
+      <div class="a-comment-date">${date}</div>
+    </div>
+    <div class="a-comment-bottom">
+      <div class="a-comment-comment">${content}</div>
+    </div>
+  `
+}
+
+
+function showComments(page_id) {
+  var a = document.getElementById(#cmtslc);
+  let filtered = [];
+  filtered = comments.filter(comment => {
+    return comment.id == page_id;
+  });
+  var something;
+  if (filtered.length == 0) {
+    var tag = document.createElement("div"); tag.className = "no-comments-box";
+    tag.innerHTML = "No comments yet";
+    a.appendChild(tag);
+  }
+  else {
+    for (var i = 0; i < filtered.length; i++) {
+      something = addPost(filtered[i].author_name, filtered[i].comment_date, filtered[i].comment_content);
+      var tag = document.createElement("div"); tag.className = "a-comment";
+      tag.innerHTML = something;
+      a.appendChild(tag);
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function filterComments(page_id) {
   let filtered = []
