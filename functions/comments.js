54ea1aed05dcc06112292adea8577f82fb4a3f8d
Keywhee <adorablefluffykitties@gmail.com>
2020-07-22 01:52:14 -0400

    Update comments.js

--- functions/comments.js
+++ functions/comments.js
@@ -2,12 +2,25 @@ const faunadb = require('faunadb'),
       q = faunadb.query;
 const client = new faunadb.Client({secret: process.env.FAUNADB_SECRET});
 
-exports.handler = function(event, context) {
+exports.handler = function(event, context, callback) {
   const data = JSON.parse(event.body);
   console.log("Function comment-create invoked", data);
   const comment = {
     data: data
   }
 
-  return client.query(q.Create(q.Collection('comments'), data));
+  return client.query(q.Create(q.Collection('comments'), data))
+    .then((response) => {
+      console.log("Success", response)
+      return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(response)
+      })
+    }).catch((error) => {
+      console.log("Error", error)
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify(error)
+      })
+    })
 };
