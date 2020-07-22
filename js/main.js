f6b8a3fb240c29b1e86122920e7715f481cd85c1
Keywhee <adorablefluffykitties@gmail.com>
2020-07-21 22:47:23 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let comments;
 
-const fetchComments = async () => {
+const fetchComments = () => {
   comments = fetch('../../comments.json')
     .then(response => response.json())
     .catch(err => err)
@@ -41,53 +41,6 @@ function showComments(page_id) {
     }
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//function filterComments(page_id) {
-//  let filtered = []
-//  filtered = comments.filter(comment => {
-//    return comment.id == page_id;
-//  });
-//  return filtered;
-//}
-
-
-//const showComments = async(comments) => {
-//  const commentsMarkup = "${renderComments(comments)}"
-//  results.innerHTML = commentsMarkup;
-//}
-
-//function renderComments(comments) {
-//  return `
-//    <ul>
-//      ${comments.map(comment => `
-//        <div class="a-comment">
-//          <div class="a-comment-top">
-//            <div class="a-comment-author">${comment.author_name}</div>
-//            <div class="a-comment-date">${comment.comment_date}</div>
-//          </div>
-//          <div class="a-comment-bottom">
-//            <div class="a-comment-comment">${comment.comment_content}</div>
-//          </div>
-//        </div>
-//        `).join('')}
-//    </ul>
-//  `
-//}
 
 (function() {
   fetchComments();
